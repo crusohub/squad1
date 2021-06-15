eeaa2bd63e94fec91eb8959bdcdcff97dce290c2
totemarcal <totemarcal@gmail.com>
2021-06-15 11:53:47 -0300

    tela project

--- src/routes.js
+++ src/routes.js
@@ -22,6 +22,7 @@ import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
 import Icons from "views/examples/Icons.js";
+import EditProject from "views/examples/EditProject.js"
 
 var routes = [
   {
@@ -72,6 +73,13 @@ var routes = [
     icon: "ni ni-circle-08 text-pink",
     component: Register,
     layout: "/auth",
+  },
+  {
+    path: "/editproject",
+    name: "Editar Projeto",
+    icon: "ni ni-circle-08 text-pink",
+    component: EditProject,
+    layout: "/admin",
   },
 ];
 export default routes;
