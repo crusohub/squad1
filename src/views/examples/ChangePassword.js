3d2559866d68d99b2a2955abe8c588fdbeb76c02
William Sodré <71849007+william0035@users.noreply.github.com>
2021-06-20 11:04:57 -0300

    forgotpass

--- src/views/examples/ChangePassword.js
+++ src/views/examples/ChangePassword.js
@@ -16,6 +16,7 @@
 
 */
 import React, { useState } from "react";
+import Data from '../../service/UserDataService'
 
 // reactstrap components
 import {
@@ -31,9 +32,80 @@ import {
 } from "reactstrap";
 
 
-const ForgotPassword = () => {
+const ChangePassword = () => {
+
+   
+      
 
   const [submit , setSubmit] = useState(false);
+  const [password, setPassword] = useState("")
+  const [passwordValid, setPasswordValid] = useState(password)
+  const [passwordVerific, setPasswordVerific] = useState(false)
+  const [compare, setCompare] = useState("")
+  const [compareVerific, setCompareVerific] = useState(false)
+  const [valuePassword , setValuePassword] = useState("")
+  const [user , setUser] = useState("")
+
+  const HandlePassword = (e) => {
+    const { name, value } = e.target
+    setPasswordValid({ ...passwordValid, [name]: value })
+    const strength = document.querySelector(".strength")
+    let pass = e.target.value
+    setValuePassword(pass)
+    if (pass.length <= 7) {
+      setPasswordVerific(false)
+      setPassword("Weak")
+      strength.classList.add("text-danger")
+      strength.classList.remove("text-success")
+    } else {
+      setPasswordVerific(true)
+      setPassword("Strong")
+      strength.classList.add("text-success")
+      strength.classList.remove("text-danger")
+    }
+    if(pass.length == 0)
+    setPassword("")
+    
+    Data.getUserByUsersName("DuAzes")//"DuAzes"
+    .then(res => {
+      const user = res.data  
+      console.log(user)
+    })
+  }
+
+  const HandleConfirm = (e) =>{
+    const { name, value } = e.target
+    setPasswordValid({ ...passwordValid, [name]: value })
+    const confirm = document.querySelector(".confirm")
+    let pass = e.target.value
+    if (pass == valuePassword) {
+        setCompareVerific(false)
+        setCompare("Compatible Password")
+        confirm.classList.add("text-success")
+        confirm.classList.remove("text-danger")
+    } else {
+        setCompareVerific(true)
+        setCompare("Unsupported passwords")
+        confirm.classList.add("text-danger")
+        confirm.classList.remove("text-success")
+    }
+    if(pass.length == 0)
+    setCompare("")
+  }
+
+  const HandleUser = (e) =>{
+    const { name, value } = e.target
+    setPasswordValid({ ...passwordValid, [name]: value })
+    const user = document.querySelector(".user")
+    let pass = e.target.value
+    setUser(pass)
+    
+  }
+
+  function SubmitPassword(){
+    
+    
+  }
 
   return (
     <>
@@ -53,7 +125,7 @@ const ForgotPassword = () => {
       <Card className="bg-secondary shadow border-0">
         <CardBody className="px-lg-5 py-lg-5">
           <div className="text-center text-muted mb-4">
-            <small>Forgot Password ?</small>
+            <small>Change Password</small>
           </div>
           <Form role="form">
           <FormGroup className="mb-3">
@@ -64,10 +136,13 @@ const ForgotPassword = () => {
                     </InputGroupText>
                   </InputGroupAddon>
                   <Input
+                    className="user"
+                    value={user}
                     name="login"
                     placeholder="User Login"
                     type="text"
                     autoComplete="new-login"
+                    onChange={HandleUser}
                   />
                 </InputGroup>
               </FormGroup>
@@ -79,12 +154,21 @@ const ForgotPassword = () => {
                     </InputGroupText>
                   </InputGroupAddon>
                   <Input
-                    name="input_new_password"
+                    name="password"
                     placeholder="New Password "
                     type="password"
                     autoComplete="new-password"
+                    onChange={HandlePassword}
                   />
                 </InputGroup>
+
+                <div className="text-muted font-italic">
+                    <small>
+                      password strength:{" "}
+                      <span className="strength font-weight-700">{password}</span>
+                    </small>
+                </div>
+
               </FormGroup>
               <FormGroup>
                 <InputGroup className="input-group-alternative">
@@ -94,16 +178,27 @@ const ForgotPassword = () => {
                     </InputGroupText>
                   </InputGroupAddon>
                   <Input
-                    name="input_confirm_password"
+                    name="confirm_password"
                     placeholder="Confirm Password"
                     type="password"
                     autoComplete="new-password"
+                    onChange={HandleConfirm}
                   />
                 </InputGroup>
+                <div className="text-muted font-italic">
+                    <small>
+                      {" "}
+                      <span className="confirm font-weight-700">{compare}</span>
+                    </small>
+                </div>
               </FormGroup>
               <div className="text-center">
-              <Input className="my-4" color="primary" type="submit">
-                Send
+              <Input 
+                className="my-4" 
+                color="primary" 
+                type="submit"
+                onSubmit={SubmitPassword}>
+                Change
               </Input>
             </div>
             </Form>
@@ -116,4 +211,4 @@ const ForgotPassword = () => {
   );
 };
 
-export default ForgotPassword;
+export default ChangePassword;
